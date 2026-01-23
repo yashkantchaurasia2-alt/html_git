6aeccf2650f413a04039cc9630b06f0b63cf7551
yashkantchaurasia2-alt <yashkantchaurasia2@gmail.com>
2026-01-23 12:24:06 +0530

    feat:added timer code

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-const form=document.querySelector("form")
-form.addEventListener('submit', (event)=>{
-    event.preventDefault()
-    console.log("button clicked")
-})
+// const form=document.querySelector("form")
+// form.addEventListener('submit', (event)=>{
+//     event.preventDefault()
+//     console.log("button clicked")
+// })
+//  var a=3453
+//  let b=456;
+
+//  console.log(a)
+//  console.log(b)
+
+//  function print(){
+//     console.log("inside fn")
+//  }
+ 
+//  print()
+
+// console.log("This is the beginning of the code.")
+// setTimeout(() =>{console.log("After 2 seconds")},2000);
+// console.log("This is the end of the code.")
+
+
+// setTimeout(()=>{console.log("Tring tring tringg, times up")},5000)
+
+// const interval=setInterval(()=>{console.log("Printing at every twoo seconds...")},2000)
+// console.log(interval)
+
+// const interval2=setInterval(()=>{console.log("Printing at every twoo seconds...")},2000)
+// console.log(interval2)
+
+// const interval=setInterval(()=>{console.log("Printing at every twoo seconds...")},2000)
+// setTimeout(()=>{clearInterval(interval)},10000)
+
+// const timeout=setTimeout(()=>{console.log("after 2 seconds...")},2000)
+// clearTimeout(timeout)
+
+// let count=0;
+// const value=setInterval(()=>{
+//     count+=1
+//     console.log(count)
+//     if(count===10){
+//         clearInterval(value)
+//     }
+// },1000)
